1ba0ca4dd56097590d4b13b7ba0f8577ce068b7b
filipchukdv <filipchukdv@gmail.com>
2023-04-21 22:05:12 +0300

    додано компонент Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import css from 'App.module.css';
+import Searchbar from './Searchbar/Searchbar';
+
+export class App extends Component {
+  onSubmit = () => console.log('submit');
+
+  render() {
+    return (
+      <div className={css.App}>
+        <Searchbar onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,23 @@
+import css from './Searchbar.module.css';
+
+const Searchbar = ({ onSubmit }) => {
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={onSubmit}>
+        <button type="submit" className="button">
+          <span className={css.SearchFormButton}>Search</span>
+        </button>
+
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
